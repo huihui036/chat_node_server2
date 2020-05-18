d025f13bf162959c28d6a93d6f7a56d0d23ab19d
huihui036 <1315574336@qq.com>
2020-05-18 20:05:58 +0800

    联系人

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = new Koa()
 
 
 
-
 app.use(cors({
     origin:"*"
 }));
@@ -21,13 +20,11 @@ const path = require('path')
 const paraser = require('koa-bodyparser')
 const InitManager = require('./core/init')
 
-
 app.use(paraser())
  require('./app/module/emoji')
 InitManager.initCore(app)
 
 // 指定 public目录为静态资源目录，存放 images
 app.use(staticFiles(path.resolve(__dirname, "./public")))
-
 
 app.listen(3001)
